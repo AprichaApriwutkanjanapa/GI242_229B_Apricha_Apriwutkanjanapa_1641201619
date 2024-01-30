e07ced0a666f12d38d245ebca15aa04d524df0e9
AprichaApriwutkanjanapa <apricha.apri@bumail.net>
2024-01-31 01:25:00 +0700

    GI242_Project

---
+++ Assets/Script/MainUI.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainUI : MonoBehaviour
+{
+    public static MainUI instance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
